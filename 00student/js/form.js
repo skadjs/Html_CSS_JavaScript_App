3dc1544f225e723b01aa2eb83574c3b49c43a29c
NamEon <143885031+skadjs@users.noreply.github.com>
2025-08-27 00:44:07 +0900

    commit

--- 00student/js/form.js
+++ 00student/js/form.js
@@ -7,21 +7,21 @@ const studentTableBody = document.getElementById("studentTableBody");
 
 //Document Load 이벤트 처리하기
 document.addEventListener("DOMContentLoaded", function () {
-    LoadStudent();
+    loadStudents();
 });
+//StudentForm 의 Submit 이벤트 처리하기
+studentForm.addEventListener("submit", function (event) {
+    //기본으로 설정된 Event가 동작하지 않도록 하기 위함
+    event.preventDefault();
+    console.log("Form 이 체출 되었음....")
 
-//StudentForm의 submit 이벤트 처리하기
-studentForm.addEventListener("submit", function () {
-    event.preventDefault(); //기본으로 설정된 Event의 실행을 막음.
-    console.log("Form이 제출되었음...");
-
-    //FormData 객체 생성: <form>엘리먼트를 객체로 변환
+    //FormData 객체생성 <form>엘리먼트를 객체로 변환
     const stuFormData = new FormData(studentForm);
-    stuFormData.forEach((value, key) => {
-        console.log(key + ' = ' + value);
-    });
+    // stuFormData.forEach((value, key) => {
+    //     console.log(key + ' = ' + value);
+    // });
 
-    //사용자 정의 Student Object Literal 객체 생성 (공백 제거 trim())
+    //사용자 정의 Student Object Literal 객체생성 (공백 제거 trim())
     const studentData = {
         name: stuFormData.get("name").trim(),
         studentNumber: stuFormData.get("studentNumber").trim(),
@@ -30,11 +30,16 @@ studentForm.addEventListener("submit", function () {
         email: stuFormData.get("email").trim(),
         dateOfBirth: stuFormData.get("dateOfBirth"),
     }
+
     //유효성 체크하는 함수 호출하기
-    if(!validateStudent(studentData)) {
+    if (!validateStudent(studentData)) {
         //검증체크 실패하면 리턴하기
         return;
     }
+
+    //유효한 데이터 출력하기
+    console.log(studentData);
+
 }); //submit 이벤트
 
 //입력항목의 값의 유효성을 체크하는 함수
@@ -85,9 +90,53 @@ function validateStudent(student) {// 필수 필드 검사
     }
 
     return true;
-} //validateStudent
+}//validateStudent
 
-//Student(학생) 목록을 Load하는 함수
-function LoadStudent() {
-    console.log("학생 목록 Load 중...");
-}
+// 이메일 유효성 검사
+function isValidEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}//isValidEmail
+
+//Student(학생) 목록을 Load 하는 함수
+function loadStudents() {
+    console.log("학생 목록 Load 중.....");
+    fetch(`${API_BASE_URL}/api/students`) //Promise
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("학생 목록을 불러오는데 실패했습니다!.");
+            }
+            return response.json();
+        })
+        .then((students) => renderStudentTable(students))
+        .catch((error) => {
+            console.log("Error: " + error);
+            alert("학생 목록을 불러오는데 실패했습니다!.");
+        });
+};
+
+function renderStudentTable(students) {
+    console.log(students);
+    studentTableBody.innerHTML = "";
+    // [{},{},{}] [] - students, {} - student
+    students.forEach((student) => {
+        //<tr> 엘리먼트를 생성하기 <tr><td>홍길동</td><td>aaa</td></tr>
+        const row = document.createElement("tr");
+
+        //<tr>의 content을 동적으로 생성
+        row.innerHTML = `
+                    <td>${student.name}</td>
+                    <td>${student.studentNumber}</td>
+                    <td>${student.detail ? student.detail.address : "-"}</td>
+                    <td>${student.detail?.phoneNumber ?? "-"}</td>
+                    <td>${student.detail?.email ?? "-"}</td>
+                    <td>${student.detail?.dateOfBirth ?? "-"}</td>
+                    <td>
+                        <button class="edit-btn" onclick="editStudent(${student.id})">수정</button>
+                        <button class="delete-btn" onclick="deleteStudent(${student.id})">삭제</button>
+                    </td>
+                `;
+        //<tbody>의 아래에 <tr>을 추가시켜 준다.
+        studentTableBody.appendChild(row);
+    });
+}//renderStudentTable
